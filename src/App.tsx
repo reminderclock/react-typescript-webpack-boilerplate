901cf5a9acec6ec17ef7ea4dee4a83ee92fcfa0c
Luke <reminderclock@naver.com>
2021-10-12 01:21:43 +0900

    Steps ~ end

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import "./styles.css";
-import IMAGE from "./react.png";
-import LOGO from "./logo.svg";
-import ClickCounter from "./ClickCounter";
-export const App = () => {
-  return (
-    <>
-      <h1>
-        Edited React TypeScript Webpack Starter Template -{" "}
-        {process.env.NODE_ENV}
-        {process.env.name}
-      </h1>
-      <img src={IMAGE} alt="React Logo" width="300" height="200"></img>
-      <img src={LOGO} alt="React Logo" width="300"></img>
-      <ClickCounter />
-    </>
-  );
+import styled from "styled-components";
+
+const App = () => {
+  return <AppWrapper></AppWrapper>;
 };
+
+const AppWrapper = styled.div``;
+
+export default App;
