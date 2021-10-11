e399e1ea3bbcf0289aee9fc28bb1c8af3cdf5f4f
Luke <reminderclock@naver.com>
2021-10-11 16:49:21 +0900

    Steps 21 to 28

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@ import LOGO from "./logo.svg";
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>;
-      <img src={IMAGE} alt="React Logo" width="300" height="200"></img>
+      <h1>
+        React TypeScript Webpack Starter Template - {process.env.NODE_ENV}
+        {process.env.name}
+      </h1>
+      ;<img src={IMAGE} alt="React Logo" width="300" height="200"></img>
       <img src={LOGO} alt="React Logo" width="300"></img>
     </>
   );
