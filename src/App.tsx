5fdacfcafe5c83988e52aabfa75c88acb3665b01
Luke <reminderclock@naver.com>
2021-10-11 14:48:09 +0900

    Steps 15 to 23

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import "./styles.css";
+import IMAGE from "./react.png";
+import LOGO from "./logo.svg";
 export const App = () => {
-  return <h1>React TypeScript Webpack Starter Template</h1>;
+  return (
+    <>
+      <h1>React TypeScript Webpack Starter Template</h1>;
+      <img src={IMAGE} alt="React Logo" width="300" height="200"></img>
+      <img src={LOGO} alt="React Logo" width="300"></img>
+    </>
+  );
 };
