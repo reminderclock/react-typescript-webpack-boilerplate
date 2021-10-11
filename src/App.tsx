45f53fda2bbb94bc27b281cbf8956c9ba58f1e0e
Luke <reminderclock@naver.com>
2021-10-11 17:04:26 +0900

    Steps 28 to 34

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import "./styles.css";
 import IMAGE from "./react.png";
 import LOGO from "./logo.svg";
+import ClickCounter from "./ClickCounter";
 export const App = () => {
   return (
     <>
       <h1>
-        React TypeScript Webpack Starter Template - {process.env.NODE_ENV}
+        Edited React TypeScript Webpack Starter Template -{" "}
+        {process.env.NODE_ENV}
         {process.env.name}
       </h1>
-      ;<img src={IMAGE} alt="React Logo" width="300" height="200"></img>
+      <img src={IMAGE} alt="React Logo" width="300" height="200"></img>
       <img src={LOGO} alt="React Logo" width="300"></img>
+      <ClickCounter />
     </>
   );
 };
