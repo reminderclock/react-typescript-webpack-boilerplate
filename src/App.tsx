7f95e2b5e2058e86085d780f59222e92970f657b
Luke <reminderclock@naver.com>
2021-11-07 21:23:51 +0900

    feat:basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
 import styled from "styled-components";
-import InputFormat from "./common/InputFormat";
 
 const App = () => {
-  return (
-    <AppWrapper>
-      <InputFormat
-        type="text"
-        thousandSeparator={true}
-        placeholder="세자리"
-        value={"1234142"}
-      />
-      <InputFormat
-        type="text"
-        delimiter="-"
-        date={true}
-        placeholder="yy-mm-dd"
-        value={["2020", "1", "11"]}
-      />
-    </AppWrapper>
-  );
+  return <AppWrapper></AppWrapper>;
 };
 
 const AppWrapper = styled.div``;
